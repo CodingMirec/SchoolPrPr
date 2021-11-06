867e54040026867437cb63e9bd524d9ab7854820
CodingMirec <m.sykora01@gmail.com>
2021-11-06 02:43:03 +0100

    zadefinovanie funkcii

--- Projekt/projekt.c
+++ Projekt/projekt.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void functionV(FILE **zoznam);
+
+void functionV(FILE **zoznam)
+{
+    int num;
+    *zoznam = fopen("konferencny_zoznam.txt", "r+");
+    if (*zoznam == NULL)
+    {
+        printf("Neotvoreny subor \n");
+        exit(1);
+    }
+    printf("Enter num: \n");
+    scanf("%d", &num);
+    fprintf(*zoznam, "%d", num);
+    fclose(*zoznam);
+    return NULL;
+}
+
+void functionO(FILE **zoznam)
+{
+}
+
+void functionN(FILE **zoznam)
+{
+}
+
+void functionS(FILE **zoznam)
+{
+}
+
+void functionH(FILE **zoznam)
+{
+}
+
+void functionP(FILE **zoznam)
+{
+}
+
+void main()
 {
     FILE *zoznam;
-    int num;
     char input;
-    printf("Napis znak: ");
+    char result;
+    printf("Napis znak: \n");
     scanf("%c", &input);
-    printf("\nNapisane : %c", input);
+
+    printf("Napisane : %c \n", input);
     if (input == 'v')
     {
-        zoznam = fopen("C:\\Users\\Mirec\\Documents\\FIIT\\PrPr cvicenia\\Projekt\\konferencny_zoznam.txt", "r+");
-        if (zoznam == NULL)
-        {
-            printf("\nNeotvoreny subor");
-            exit(1);
-        }
-        printf("\nEnter num: ");
-        scanf("%d", &num);
-        fprintf(zoznam, "%d", num);
-        fclose(zoznam);
-        return 0;
+        functionV(&zoznam);
+        return NULL;
     }
 
-    printf("\nZly vstup");
+    printf("Zly vstup \n");
 }
