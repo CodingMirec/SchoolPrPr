05f37a237ace8afaad88082b784df155df1edc28
CodingMirec <m.sykora01@gmail.com>
2021-11-06 03:27:29 +0100

    switch in V

--- Projekt/projekt.c
+++ Projekt/projekt.c
@@ -3,20 +3,66 @@
 
 void functionV(FILE **zoznam);
 
+int rodneIsValid(char *str)
+{
+    unsigned long long value = strtoull(str, NULL, 10);
+    return value >= 1000000000 && value % 11 == 0;
+}
+
 void functionV(FILE **zoznam)
 {
-    int num;
-    *zoznam = fopen("konferencny_zoznam.txt", "r+");
+    char slovo, array;
+    int i, num;
+
+    *zoznam = fopen("konferencny_zoznam.txt", "r");
     if (*zoznam == NULL)
     {
         printf("Neotvoreny subor \n");
         exit(1);
     }
-    printf("Enter num: \n");
-    scanf("%d", &num);
-    fprintf(*zoznam, "%d", num);
-    fclose(*zoznam);
-    return NULL;
+    printf("otvorene");
+
+    slovo = getc(*zoznam);
+    ungetc(slovo, *zoznam);
+    for (i = 1; i != EOF; i++)
+    {
+        while (fgets(array, 200, *zoznam) != NULL)
+        {
+            switch (i)
+            {
+            case '1':
+                printf("Prezenter: %s", array);
+                break;
+            case '2':
+                printf("Rodne cislo: %s", array);
+                break;
+            case '3':
+                printf("Kod prezentacnej miestnosti: %s", array);
+                break;
+            case '4':
+                printf("Nazov prispevku: %s", array);
+                break;
+            case '5':
+                printf("Mena autorov: %s", array);
+                break;
+            case '6':
+                printf("Typ prezentovania: %s", array);
+                break;
+            case '7':
+                printf("Cas prezentovania: %s", array);
+                break;
+            case '8':
+                printf("Datum: %s", array);
+                break;
+
+            default:
+                printf("something went wrong");
+                break;
+            }
+        }
+    }
+
+    exit(1);
 }
 
 void functionO(FILE **zoznam)
@@ -36,6 +82,14 @@ void functionH(FILE **zoznam)
 }
 
 void functionP(FILE **zoznam)
+{
+}
+
+void functionZ(FILE **zoznam)
+{
+}
+
+void functionK(FILE **zoznam)
 {
 }
 
